fe86b07248060dd277b2ea04fb3a0aca1bad459b
Ajay12Ajay <ajaystl3@gmail.com>
2026-01-10 15:39:31 +0530

    10-Jan-26

---
+++ spring-core-project/src/main/java/com/rays/lifecycle/MyBean.java
@@ -0,0 +1,17 @@
+package com.rays.lifecycle;
+
+public class MyBean {
+
+	public void init() {
+
+		System.out.println("..bean initialized..");
+
+	}
+
+	public void destroy() {
+
+		System.out.println("..bean closed..");
+
+	}
+
+}
